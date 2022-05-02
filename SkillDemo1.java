7072160f3263a2befa2c1dc1d069af6d114c4cf4
andymachorro <103210240+andymachorro@users.noreply.github.com>
2022-05-01 21:03:45 -0700

    commit some

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,4 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
 public class  SkillDemo1{
+
+    public static int add (int a, int b){
+        return a+b;
+    }
+    @Test
+    public void additionCheck(){
+        assertEquals(4, SkillDemo1.add(2,1));
+    }    
+
 
 
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void additionCheck(){
+        assertEquals(4, SkillDemo1.add(2,1));
+    }    
+}
